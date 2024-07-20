2681a4e371a4143ea25e46b76dd503254b1bbe20
Kris Papercut <krispcut@gmail.com>
2024-07-20 14:37:56 +0300

    v1.0.13

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,4 @@
 export * from "./ElasticSearchPlugin";
-export * from "./LocaltunnelPlugin";
-export * from "./MaildevPlugin";
 export * from "./MongodbPlugin";
 export * from "./PageKitePlugin";
 export * from "./ProxmoxPlugin";
